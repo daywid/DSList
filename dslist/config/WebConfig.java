08cf18bf2b5cd9958a2514daabd88c18652de393
Daywid <88506180+daywid@users.noreply.github.com>
2023-07-13 17:39:16 -0300

    Cors config

---
+++ dslist/config/WebConfig.java
@@ -0,0 +1,17 @@
+@Configuration
+public class WebConfig {
+
+	@Value("${cors.origins}")
+	private String corsOrigins;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
+	}
+	
+}
