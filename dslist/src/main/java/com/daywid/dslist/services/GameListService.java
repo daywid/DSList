136d137c9b267cd80930596de982159af323e326
Daywid <88506180+daywid@users.noreply.github.com>
2023-07-14 14:35:47 -0300

    Move games in list

--- dslist/src/main/java/com/daywid/dslist/services/GameListService.java
+++ dslist/src/main/java/com/daywid/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.daywid.dslist.dto.GameListDTO;
 import com.daywid.dslist.entities.GameList;
+import com.daywid.dslist.projections.GameMinProjection;
 import com.daywid.dslist.repositories.GameListRepository;
+import com.daywid.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,11 +18,28 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
         public List<GameListDTO> find_all(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
-        
+    }
+
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex: destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex: sourceIndex;
+
+        for(int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 
 }
